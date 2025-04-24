eaefff097fccf8e008df896ff71016964e056ab5
Praccho Muna-McQuay <pracchommc@gmail.com>
2025-04-23 23:44:40 -0400

    train script, save conf :)

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 import os
 import datetime
+import shutil
 
 from argparse import ArgumentParser
 from omegaconf import OmegaConf
+
+LOG_PARENT = "logs"
 
 def main(args):
     # loading conf
@@ -10,10 +13,15 @@ def main(args):
 
     # set up logdir
     cfg_fname = os.path.splitext(os.path.split(args.config)[-1])[0]
-    now = datetime.datetime.now().strftime("%dT%H-%M-%S")
-    nowname = now + "_" + cfg_fname
-    logdir = os.path.join("logs", nowname)
+    if args.name:
+        name = args.name
+    else:
+        now = datetime.datetime.now().strftime("%m-%d-T%H-%M-%S")
+        name = now + "_" + cfg_fname
+        
+    logdir = os.path.join(LOG_PARENT, name)
     os.makedirs(logdir, exist_ok=True)
+    shutil.copy(args.config, os.path.join(logdir, os.path.basename(args.config)))
 
     # more shenanigans...
 
@@ -24,7 +32,6 @@ if __name__ == '__main__':
     parser.add_argument(
         "-n",
         "--name",
-        required=True,
         type=str,
         help="experiment name"
     )
