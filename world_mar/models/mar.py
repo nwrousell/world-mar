cd7fe66ac4b121fb7b93578ee2244adc8140b1d4
Praccho <pracchommc@gmail.com>
2025-04-26 18:42:09 +0000

    mar updates

--- world_mar/models/mar.py
+++ world_mar/models/mar.py
@@ -36,19 +36,42 @@ class WorldMAR(pl.LightningModule):
     """
     def __init__(self, 
                  vae_config, # should be an AutoencoderKL 
-                 img_height=360, img_width=640,
+                 img_height=360, img_width=640, num_frames=8,
                  encoder_embed_dim=512, encoder_depth=16, encoder_num_heads=16,
                  decoder_embed_dim=512, decoder_depth=16, decoder_num_heads=16,
-                 warmup_steps=10000 # TODO: change this depending on dataset size
+                 mask_ratio_min=0.7,
+                 proj_dropout=0.1,
+                 attn_dropout=0.1,
+                 warmup_steps=10000, # TODO: change this depending on dataset size
+                 **kwargs
     ):
         super().__init__()
         self.automatic_optimization = False
 
-        # intialize the vae
+        # --- intialize the vae ---
         self.instantiate_vae(vae_config)
         assert isinstance(self.vae, AutoencoderKL)
+        self.seq_h, self.seq_w, self.seq_t = self.vae.seq_h, self.vae.seq_w, num_frames
+        self.seq_len = self.seq_h * self.seq_w * self.seq_t
+        self.token_embed_dim = self.vae.latent_dim
+
+        # --- masking statistics ---
+        # ref: masking ratio used by MAR for image gen
+        self.mask_ratio_gen = stats.truncnorm((mask_ratio_min -1.0) / 0.25, 0, loc=1.0, scale=0.25)
+
+        # --- encoder ---
+        self.z_proj = nn.Linear(self.token_embed_dim, encoder_embed_dim, bias=True) # projs VAE latents to transformer dim
+        self.z_proj_ln = nn.LayerNorm(encoder_embed_dim, eps=1e-6)
+        self.encoder_blocks = nn.ModuleList([
+            Block(encoder_embed_dim, encoder_num_heads, qkv_bias=True,
+                  proj_drop=proj_dropout, attn_drop=attn_dropout) for _ in range(encoder_depth)])
+        self.encoder_norm = nn.LayerNorm(encoder_embed_dim)
+
+        # --- decoder ---
+        self.decoder_embed = nn.Linear(encoder_embed_dim, decoder_embed_dim, bias=True)
+        self.mask_token = nn.Parameter(torch.zeros(1,1, decoder_embed_dim))
         
-        # initialize diff loss
+        # --- initialize diff loss ---
         # TODO: make cutomizable as MLP (per patch?) vs DiT (per frame).
         #       for now, assuming more lightweight MLP
         # self.diffloss = DiffLoss
